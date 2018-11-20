e0a2929f919133a031bf62de9fae6fa8c346b0eb
maxminor <peace_under@hotmail.com>
2018-11-20 18:19:14 +0700

    fix bugs for SLS command

--- adhoc.py
+++ adhoc.py
@@ -20,7 +20,7 @@ import udpSender
 #SLS: return from LST
 class UDPAdHoc:
     def __init__(self, ip, port):
-        self.network_name = 'network1'
+        self.network_name = ''
         self.UDP_IP = ip
         self.UDP_PORT = port
         self.sock = socket.socket(socket.AF_INET, # Internet
@@ -40,7 +40,7 @@ class UDPAdHoc:
                 cleaned_data = data.decode('utf-8').strip()
                 print("received message:", cleaned_data)
                 command = cleaned_data[0:3]
-                print(command)
+                # print(command)
                 if command == 'ADD':
                     new_song = cleaned_data[4:]
                     print(new_song)
@@ -50,16 +50,23 @@ class UDPAdHoc:
                         self.song_data[str(new_song)] = 1
                 elif command == 'LST':
                     if (addr != socket.gethostbyname(socket.gethostname()) and self.network_name != ''):
-                        print('sending new list...')
+                        # print('sending new list...')
                         newdata = {'network_name': self.network_name, 'song_data': self.song_data}
                         payload = 'SLS ' + json.dumps(newdata)
-                        print(addr[0])
+                        # print(addr[0])
                         udpSender.sendUDPPacket(str(addr[0]), 5000, payload)
-                elif cleaned_data.split()[0] == 'SLS':
-                    received_payload = cleaned_data[4:]
-                    newdict = json.loads(received_payload)
-                    self.network_name = newdict['network_name']
-                    self.data = newdict['song_data']
+                elif command == 'SLS':
+                    print(self.network_name)
+                    if(addr != socket.gethostbyname(socket.gethostname()) and self.network_name == ''):
+                        received_payload = cleaned_data[4:]
+                        # print('payload is: ', received_payload)
+                        try:
+                            newdict = json.loads(received_payload)
+                            print(newdict)
+                        except e:
+                            print(e)
+                        self.network_name = newdict['network_name']
+                        self.song_data = newdict['song_data']
 
                 # for song in self.song_data.keys():
                 #     print("{}: {}".format(str(song), self.song_data[song]))
